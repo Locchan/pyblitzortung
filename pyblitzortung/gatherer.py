f0c4a2bc1347924694875032c6a01958ee383eb1
Jaroslaw Locchansky <locchansky@gmail.com>
2024-09-14 00:23:20 +0300

    Hotfix #4

--- pyblitzortung/gatherer.py
+++ pyblitzortung/gatherer.py
@@ -36,7 +36,7 @@ def init_database():
         cursorclass=pymysql.cursors.DictCursor
     )
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS strikes (id MEDIUMINT NOT NULL AUTO_INCREMENT, time DATETIME, lat FLOAT, lon FLOAT, PRIMARY KEY (id));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS strikes (id BIGINT NOT NULL AUTO_INCREMENT, time TIMESTAMP(6), lat FLOAT, lon FLOAT, PRIMARY KEY (id));")
     return db, cursor
 
 def deobf_message(ciphertext: str) -> str:
