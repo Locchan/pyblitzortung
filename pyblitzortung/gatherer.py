d54cab340b72d4e17e20225b7ed8b2a03317899b
Jaroslaw Locchansky <locchansky@gmail.com>
2024-09-13 21:30:36 +0300

    Version 1

---
+++ pyblitzortung/gatherer.py
@@ -0,0 +1,89 @@
+import websocket
+import random
+import json
+import sqlite3
+import datetime
+
+counter = 0
+mon_metrics = {
+    "strikes_total": 0,
+    "errors": 0,
+    "reconnects": -1
+}
+
+def init_database(path="./sb.sqlite"):
+    db = sqlite3.connect(path)
+    db.execute("CREATE TABLE IF NOT EXISTS strikes (id INTEGER PRIMARY KEY AUTOINCREMENT, time DATETIME, lat FLOAT, lon FLOAT);")
+    return db
+
+def deobf_message(ciphertext: str) -> str:
+    chars = iter(ciphertext)
+
+    c = next(chars, None)
+    if c is None:
+        return ""
+    
+    prev = c
+    out = c
+    dict = []
+
+    for char in chars:
+        code = ord(char)
+
+        a = char if code < 256 else dict[code - 256] if code - 256 < len(dict) else f"{prev}{c}"
+        out += a
+        c = a[0]
+        dict.append(f"{prev}{c}")
+        prev = a
+
+    return out
+    
+def gen_random_url():
+    known_wss = ["ws1", "ws7", "ws8"]
+    return f"wss://{random.choice(known_wss)}.blitzortung.org/"
+
+def export_monitoring_metrics():
+    with open("pyblitzortung.metrics", "w") as mon_file:
+        json.dump(mon_metrics, mon_file)
+
+def on_message(ws, message):
+    global counter
+    message = deobf_message(message)
+    message_dict = json.loads(message)
+    db.execute(f"INSERT INTO strikes (time, lat, lon) VALUES ({message_dict["time"]},{message_dict["lat"]},{message_dict["lon"]});")
+    counter += 1
+    if counter == 100:
+        counter = 0
+        mon_metrics["strikes_total"] += 100
+        print(f"{datetime.datetime.now()}: Committed 100 strikes.")
+        db.commit()
+        export_monitoring_metrics()
+    #print(f"Added a strike at lat: {strike[1]} lon: {strike[2]}")
+
+def on_error(ws, error):
+    print(f"Encountered error: {error}")
+    mon_metrics["errors"] += 1
+    connect()
+
+def on_close(ws, close_status_code, close_msg):
+    print(f"Connection closed: {close_status_code}: {close_msg}")
+    connect()
+
+def on_open(ws):
+    print("Connection opened")
+    ws.send('{"a": 111}')
+
+def connect():
+    mon_metrics["reconnects"] += 1
+    url = gen_random_url()
+    print(f"Connecting to '{url}'...")
+    ws = websocket.WebSocketApp(url,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+
+db = init_database()
+connect()
